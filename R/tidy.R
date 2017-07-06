f9671e6100a46b5d40970af5db7378194b55d116
Lorenz Walthert <lorenz.walthert@icloud.com>
2017-07-06 12:38:41 +0200

    improve

--- R/tidy.R
+++ R/tidy.R
@@ -1,30 +1,34 @@
-#' tidy current rmd file
-#' @import tidyverse
+#' tidy current rmd file to fit 80 characters a line
+#' @import dplyr
 #' @import rstudioapi
 #' @import stringr
-#' @import purrr
-tidy_active <- function() {
+#' @import readr
+#' @importFrom purrr map map2
+tidy_active_file <- function() {
   file <- rstudioapi::getActiveDocumentContext()$path
   tidy_file(file)
 }
 
 tidy_file <- function(path = "README.Rmd") {
-  one_string <- read_lines(path)
-  blanks <- str_locate_all(one_string, " ") %>%
-    map(~.x[,1]) %>%
-    map(cutting_points)
-  newlines <- one_string %>%
-    map2(blanks, split_pos) %>%
-    unlist() %>%
-    map(trimws, "right") %>%
-    paste(sep = "\n") %>%
+  read_lines(path) %>%
+    tidy_text() %>%
     write_lines(path)
+
 }
 
-cutting_points <- function(x) {
-  above <- first(which(x >= 80))
-  below <- last(which(x < 80))
-  if (length(above) == 0 || is.na(above)) return(80)
+tidy_text <- function(text) {
+  paste(text, collapse = " ") %>%
+    str_replace_all(" +", " ") %>%
+    cut_long %>%
+    unlist() %>%
+    map(trimws, "right") %>%
+    paste(sep = "\n")
+}
+
+cutting_points <- function(x, length = 80) {
+  above <- first(which(x >= length))
+  below <- last(which(x < length))
+  if (is.na(below)) return(length)
   x[below]
 }
 
@@ -34,4 +38,40 @@ split_pos <- function(string, pos) {
   if (nchar(string) <= pos) return(first)
   second <- str_sub(string, pos + 1, -1)
   c(first, second)
+}
+
+#' Tidy highlighted region
+tidy_active_region <- function() {
+  file <- rstudioapi::getActiveDocumentContext()$path
+  all <- read_lines(file)
+  selection <- rstudioapi::getActiveDocumentContext()$selection
+  start_row <- selection[[1]]$range$start[1]
+  end_row <- selection[[1]]$range$end[1]
+
+  pre <- all[seq_len(start_row - 1)]
+  post <- if (end_row %in% c(length(all), length(all) + 1)) {
+    post <- NULL
+  } else {
+    post <- all[(end_row + 1):length(all)]
+  }
+  process <- all[start_row:end_row] %>%
+    tidy_text()
+  write_lines(c(pre, process, post), file)
+
+}
+
+
+cut_long <- function(text, length = 80) {
+  if (nchar(text) < length) return(text)
+
+  blanks <- str_locate_all(text, " ")[[1]][, 1]
+
+  optimal_length <- cutting_points(blanks, length)
+  we_do <- str_sub(text, 1, optimal_length)
+  postpone <- str_sub(text, optimal_length + 1, - 1)
+  c(we_do, cut_long(postpone, length)) %>%
+      unlist() %>%
+      paste0(collapse = "\n") %>%
+    str_split_fixed("\n", n = Inf) %>%
+    as.vector()
 }
