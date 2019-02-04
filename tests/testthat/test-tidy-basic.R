ea1f8e08b2ead20c578580d06de61637b47c4bf4
lorenzwalthert <lorenz.walthert@icloud.com>
2019-02-04 20:51:42 +0100

    fixing test suite by checking for warning, not error

--- tests/testthat/test-tidy-basic.R
+++ tests/testthat/test-tidy-basic.R
@@ -1,6 +1,6 @@
 context("test-tidy-basic")
 test_that("Ordinary rmd document with header works", {
-  expect_error(
+  expect_warning(
     test_collection("basic", transformer = tidy_text),
     NA
   )
@@ -8,39 +8,39 @@ test_that("Ordinary rmd document with header works", {
 
 
 test_that("Bullet list", {
-  expect_error(
+  expect_warning(
     test_collection("bullet", transformer = tidy_text),
     NA
   )
-  expect_error(
+  expect_warning(
     test_collection("bullet", transformer = tidy_text),
     NA
   )
 })
 
 test_that("long lines", {
-  expect_error(
+  expect_warning(
     test_collection("long-lines", transformer = tidy_text),
     NA
   )
 })
 
 test_that("width argument ", {
-  expect_error(
+  expect_warning(
     test_collection("width-argument", transformer = tidy_text, width = 10),
     NA
   )
 })
 
 test_that("enumeration", {
-  expect_error(
+  expect_warning(
     test_collection("enumeration", transformer = tidy_text),
     NA
   )
 })
 
 test_that("code chunks without preceding blank", {
-  expect_error(
+  expect_warning(
     test_collection("code", "chunk-without-blank",
       transformer = tidy_text
     ),
@@ -49,21 +49,21 @@ test_that("code chunks without preceding blank", {
 })
 
 test_that("code", {
-  expect_error(
+  expect_warning(
     test_collection("code", "linebreaks", transformer = tidy_text),
     NA
   )
 })
 
 test_that("code", {
-  expect_error(
+  expect_warning(
     test_collection("code", "agnostic", transformer = tidy_text),
     NA
   )
 })
 
 test_that("code with latex $$ $$ is recognized", {
-  expect_error(
+  expect_warning(
     test_collection("code", "latex-formula", transformer = tidy_text),
     NA
   )
@@ -71,7 +71,7 @@ test_that("code with latex $$ $$ is recognized", {
 
 
 test_that("don't break title", {
-  expect_error(
+  expect_warning(
     test_collection("title", "dont-break", transformer = tidy_text),
     NA
   )
